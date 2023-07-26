584ba505bd869a5d213883a74bd430ea712ad228
Ivan Marchuk <imarchuk90@gmail.com>
2023-07-26 20:36:34 +0300

    Add components and initial router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material"; 
+import { Navbar } from "./components/Navbar";
+import { Feed } from "@mui/icons-material";
+import { VideoDetail } from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Box sx={{background: '#000'}}>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTern' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
